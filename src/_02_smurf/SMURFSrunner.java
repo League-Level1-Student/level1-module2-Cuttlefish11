e776a2648a9b3f4094ff63ddf16d37c27245ea95
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2021-05-17 18:22:01 -0700

    cydy3

---
+++ src/_02_smurf/SMURFSrunner.java
@@ -0,0 +1,13 @@
+package _02_smurf;
+
+public class SMURFSrunner {
+public static void main(String[] args) {
+	Smurf handy = new Smurf ("Handy");
+handy.eat();
+handy.getName();
+Smurf hand = new Smurf ("Papa");
+hand.getName();
+hand.getHatColor();
+hand.isGirlOrBoy();
+}
+}
